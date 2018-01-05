ffd9a7016ccca299baf03a73e4f1eba6815833ba
donank <11donank22@gmail.com>
2018-01-05 15:00:46 +0530

    Bittrex API complete

--- app/src/main/java/com/donank/tradecrypto/Api/BittrexApiService.kt
+++ app/src/main/java/com/donank/tradecrypto/Api/BittrexApiService.kt
@@ -1,10 +1,16 @@
 package com.donank.tradecrypto.Api
 
+import com.donank.tradecrypto.Data.*
 import com.squareup.moshi.Moshi
+import io.reactivex.Observable
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
 
 /**
  * Created by donank on 30/12/17.
@@ -12,6 +18,62 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 interface BittrexApiService {
 
     //api calls here
+    @GET("/public/getmarkets")
+    fun getMarkets() : Observable<Market>
+
+    @GET("/public/getcurrencies")
+    fun getCurrencies() : Observable<SupportedCurrencies>
+
+    @POST("/public/getticker")
+    fun getTicker(@Body market : String) : Observable<SpecificTickerValues>
+
+    @GET("/public/getmarketsummaries")
+    fun getMarketSummary() : Observable<MarketSummary>
+
+    @POST("/public/getmarketsummary")
+    fun getSpecificmarketSummary(@Body market : String) : Observable<MarketSummary>
+
+    @POST("/public/getorderbook")
+    fun getOrderBook(@Body market : String, type : String) : Observable<OrderBook>
+
+    @POST("/public/getmarkethistory")
+    fun getMarketHistory(@Body market : String) : Observable<SpecificMarketHistory>
+
+    @POST("/market/buylimit")
+    fun buy(@Query("apikey") apikey: String, @Query("market") market : String, @Query("quantity") quantity : Double, @Query("rate") rate : Double) : Observable<BuySellLimit>
+
+    @GET("/market/selllimit")
+    fun sell(@Query("apikey") apikey: String, @Query("market") market : String, @Query("quantity") quantity : Double, @Query("rate") rate : Double) : Observable<BuySellLimit>
+
+    @GET("/market/cancel")
+    fun cancel(@Query("apikey") apikey: String, @Query("uuid") uuid : String) : Observable<CancelResult>
+
+    @GET("/market/getopenorders")
+    fun getOpenOrders(@Query("apikey") apikey: String, @Query("market") market : String) : Observable<OpenOrders>
+
+    @GET("/account/getbalances")
+    fun getBalances(@Query("apikey") apikey : String) : Observable<AccountBalance>
+
+    @GET("/account/getbalance")
+    fun getSpecificBalance(@Query("apikey") apikey: String, @Query("currency") currency : String) : Observable<SpecificAccountBalance>
+
+    @GET("/account/getdepositaddress")
+    fun getDepositAddress(@Query("apikey") apikey : String, @Query("currency") currency : String) : Observable<DepositAddress>
+
+    @GET("/account/withdraw")
+    fun withdraw(@Query("currency") currency : String, @Query("quantity") quantity: Double, @Query("address") address : String, @Query("paymentid") paymentid : String) : Observable<Withdraw>
+
+    @GET("/account/getorder")
+    fun getOrder(@Query("uuid") uuid : String) : Observable<SingleOrder>
+
+    @GET("/account/getorderhistory")
+    fun getOrderHistory(@Query("market") market : String) : Observable<OrderHistory>
+
+    @GET("/account/getwithdrawalhistory")
+    fun getWithdrawalHistory(@Query("currency") currency: String) : Observable<TransferHistory>
+
+    @GET("/account/getdeposithistory")
+    fun getDepositHistory(@Query("currency") currency: String) : Observable<TransferHistory>
 
     companion object {
 
@@ -23,7 +85,7 @@ interface BittrexApiService {
                     .addConverterFactory(MoshiConverterFactory.create(moshi)
                             .asLenient())
                     .client(client)
-                    .baseUrl("https://bittrex.com/api/v1.1/")
+                    .baseUrl("https://bittrex.com/api/v1.1")
                     .build()
 
             return retrofit.create(BittrexApiService::class.java)
